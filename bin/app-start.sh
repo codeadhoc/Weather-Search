0196d126130939425c24d426110cbe65a3ce1209
unknown <viral.v21@gmail.com>
2021-08-14 13:39:56 +0400

    modification in app-start & header.hbs

--- bin/app-start.sh
+++ bin/app-start.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
-cp /var/www/weatherapp/weatherapp.service /etc/systemd/system
+##cp /var/www/weatherapp/weatherapp.service /etc/systemd/system
 ##add exceutable permissions to express app
-sudo chmod +x /var/www/weatherapp/src/server.js
+##sudo chmod +x /var/www/weatherapp/src/server.js
 ##Allows any users to write the app folder. Useful if using fs within the app
 sudo chmod go+w /var/www/weatherapp
 ##Launches the express app
-sudo systemctl daemon-reload
-sudo systemctl start weatherapp
-sudo systemctl enable weatherapp
+##sudo systemctl daemon-reload
+##sudo systemctl start weatherapp
+##sudo systemctl enable weatherapp
+
+##Run your nodejs app
+sudo node /var/www/weatherapp/src/app.js
